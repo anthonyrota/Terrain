172e79b35fb5a825abe33454fa12f15d04a014a0
anthonyrota <28838817+anthonyrota@users.noreply.github.com>
2021-06-28 19:01:07 +1000

    feat: 1024 chunk size

--- src/crateConstants.ts
+++ src/crateConstants.ts
@@ -1,4 +1,4 @@
-export const CHUNK_WIDTH = 512;
-export const CHUNK_DEPTH = 512;
+export const CHUNK_WIDTH = 1024;
+export const CHUNK_DEPTH = 1024;
 export const MAX_HEIGHT = 512;
 export const EROSION_OCEAN_HEIGHT = 0.37;
